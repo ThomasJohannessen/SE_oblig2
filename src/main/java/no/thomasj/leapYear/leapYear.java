1a73d68fde7f04588eff0f8be92760131591ac84
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2020-10-11 14:08:26 +0200

    Started using tests

---
+++ src/main/java/no/thomasj/leapYear/leapYear.java
@@ -0,0 +1,13 @@
+package no.thomasj.leapYear;
+
+public class leapYear {
+    public static void main(String[] args) {
+        leapYear prog = new leapYear();
+        System.out.println(prog.message());
+    }
+
+
+    public String message() {
+        return "Hello";
+    }
+}
--- src/test/java/When_using_my_software.java
+++ src/test/java/When_using_my_software.java
@@ -1,2 +1,12 @@
+import no.thomasj.leapYear.leapYear;
+import org.junit.jupiter.api.*;
+
 public class When_using_my_software {
+
+    @Test
+    public void i_get_a_nice_message(){
+        leapYear sut = new leapYear();
+        String actual = sut.message();
+        Assertions.assertEquals("Hello",actual);
+    }
 }
