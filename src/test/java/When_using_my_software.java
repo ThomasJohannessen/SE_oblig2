619835640a1eee6191898d8aea25c97bb1fe9044
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2020-10-11 14:39:05 +0200

    main functionalety ok

--- src/test/java/When_using_my_software.java
+++ src/test/java/When_using_my_software.java
@@ -2,11 +2,11 @@ import no.thomasj.leapYear.leapYear;
 import org.junit.jupiter.api.*;
 
 public class When_using_my_software {
-
+/*
     @Test
     public void i_get_a_nice_message(){
         leapYear sut = new leapYear();
-        String actual = sut.message();
+        Boolean actual = sut.isLeapYear();
         Assertions.assertEquals("Hello",actual);
-    }
+    }*/
 }
